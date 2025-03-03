d2707afd86feb54300180aecdf31155986b17dce
Milon756 <70418004+Milon756@users.noreply.github.com>
2025-03-03 06:30:05 +0600

    DailyNexus.js

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
-const body = document.querySelector("body"),
-      nav = document.querySelector("nav"),
-      modeToggle = document.querySelector(".dark-light"),
-      searchToggle = document.querySelector(".searchToggle"),
-      sidebarOpen = document.querySelector(".sidebarOpen"),
-      siderbarClose = document.querySelector(".siderbarClose");
+// Toggle mobile sidebar
+        document.getElementById('menu-btn').addEventListener('click', function () {
+            document.getElementById('sidebar').classList.add('show');
+        });
 
-      let getMode = localStorage.getItem("mode");
-          if(getMode && getMode === "dark-mode"){
-            body.classList.add("dark");
-          }
+        document.getElementById('close-btn').addEventListener('click', function () {
+            document.getElementById('sidebar').classList.remove('show');
+        });
 
-// js code to toggle dark and light mode
-      modeToggle.addEventListener("click" , () =>{
-        modeToggle.classList.toggle("active");
-        body.classList.toggle("dark");
-
-        // js code to keep user selected mode even page refresh or file reopen
-        if(!body.classList.contains("dark")){
-            localStorage.setItem("mode" , "light-mode");
-        }else{
-            localStorage.setItem("mode" , "dark-mode");
-        }
-      });
-
-// js code to toggle search box
-        searchToggle.addEventListener("click" , () =>{
-        searchToggle.classList.toggle("active");
-      });
- 
-      
-//   js code to toggle sidebar
-sidebarOpen.addEventListener("click" , () =>{
-    nav.classList.add("active");
-});
-
-body.addEventListener("click" , e =>{
-    let clickedElm = e.target;
-
-    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
-        nav.classList.remove("active");
-    }
-});
-
-
+        // Toggle mobile dropdown menu
+        document.getElementById('mobile-dropdown-btn').addEventListener('click', function () {
+            var dropdown = document.getElementById('mobile-dropdown');
+            if (dropdown.style.display === "block") {
+                dropdown.style.display = "none";
+            } else {
+                dropdown.style.display = "block";
+            }
+        });
